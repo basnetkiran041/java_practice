de12fad708206eb9b1f9ad41bb85a01952948edd
Kiran Basnet <basnetkiran041@gmail.com>
2025-05-05 16:35:11 +0545

    access_control

---
+++ Access_Modifiers/src/accessDefault.java
@@ -0,0 +1,14 @@
+class Student{
+    void show()
+    {
+        System.out.println("I am a  BCA student");
+    }
+}
+
+public class accessDefault {
+    public static void main(String[] args) {
+        Student st = new Student();
+        st.show();
+        
+    }
+}
---
+++ Access_Modifiers/src/accessPrivate.java
@@ -0,0 +1,22 @@
+public class accessPrivate {
+    public static void main(String[] args) {
+        myClass ref = new myClass();
+        ref.show();
+        
+    }
+    
+}
+
+class myClass
+{
+    private int a = 100;
+    private void display()
+    {
+        System.out.println("Private value of a is :"+a);
+    }
+    void show()
+    {
+        display();
+        System.out.println("Accessing private value of a :" +a);
+    }
+}
